bef1b80132ffcc646b891d4fed00c1f621d0c35c
ikimodus <ifhumail@gmail.com>
2026-06-10 00:04:24 +0200

    Add basic calc btns And start of calc logic

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let currentNum = 0;
+const displayCalc = document.querySelector(".calc-display");
+const digits = document.querySelectorAll(".calc-digit");
+
+// basic math
 function add(a, b) {
   return a + b;
 }
@@ -10,3 +15,30 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+//
+function operate(a, b, operator) {
+  const result =
+    operator === "+"
+      ? add(a, b)
+      : operator === "-"
+        ? subtract(a, b)
+        : operator === "*"
+          ? multiply(a, b)
+          : operator === "/"
+            ? divide(a, b)
+            : "ERROR! : Invalid Operator";
+  return result;
+}
+function updateDisplay(str) {
+  displayCalc.textContent = str;
+  return str;
+}
+
+digits.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.textContent === "Clear") {
+      updateDisplay("0");
+      currentNum = 0;
+    }
+  });
+});
